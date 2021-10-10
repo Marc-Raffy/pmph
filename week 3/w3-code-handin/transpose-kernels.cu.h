eb39192fa2321fd3b2428fe24587177598b231ce
Marc André Raymond Raffy <kpf905@alumni.ku.dk>
2021-10-10 16:25:37 +0200

    qszrxtdfvgjnl;ù!

--- week 3/w3-code-handin/transpose-kernels.cu.h
+++ week 3/w3-code-handin/transpose-kernels.cu.h
@@ -79,13 +79,11 @@ transfProg(float* Atr, float* Btr, unsigned int N) {
     unsigned int gid = (blockIdx.x * blockDim.x + threadIdx.x);
     if(gid >= N) return;
     float accum = 0.0;
-    unsigned int thd_offs = gid * 64;
 
     for(int j=0; j<64; j++) {
-        //gid + j*N
-        float tmpA  = Atr[thd_offs + j];
+        float tmpA  = Atr[gid + j*N];
         accum = sqrt(accum) + tmpA*tmpA;
-        Btr[thd_offs + j]  = accum;
+        Btr[gid + j*N]  = accum;
     }
 }
 
