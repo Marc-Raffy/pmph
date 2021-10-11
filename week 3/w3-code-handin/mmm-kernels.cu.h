7fe3d8aca4a524b46ce0db81614561094d05ac74
Marc André Raymond Raffy <kpf905@alumni.ku.dk>
2021-10-11 18:39:47 +0200

    v

--- week 3/w3-code-handin/mmm-kernels.cu.h
+++ week 3/w3-code-handin/mmm-kernels.cu.h
@@ -91,6 +91,7 @@ __global__ void matMultRegTiledKer(ElTp* A, ElTp* B, ElTp* C, int heightA, int w
         Ash[threadIdx.y][threadIdx.x] = ((ii + threadIdx.y < heightA) && (kk+threadIdx.x < widthA)) ?
           A[(ii + threadIdx.y)*widthA + kk + threadIdx.x] : 0.0;
         __syncthreads();
+        #pragma unroll
         for(int k = 0; k< widthA - kk; k++){
           if(jj < widthB && j < widthB){
             float b = B[k, j];
@@ -104,6 +105,7 @@ __global__ void matMultRegTiledKer(ElTp* A, ElTp* B, ElTp* C, int heightA, int w
       }
       if(ii < heightA and jjj < widthB){
       {
+        #pragma unroll
         for(int i=0;i<heightA-ii;i++){
           C[i,j] = cs[i];
         }
