f69c2ee8005d2b60eb8e84570b9df312ae4cd499
Marc André Raymond Raffy <kpf905@alumni.ku.dk>
2021-10-25 18:58:50 +0200

    sfb

--- Project code/main.cu
+++ Project code/main.cu
@@ -11,11 +11,17 @@ int main()
     unsigned int* h_in_rand = new unsigned int[num_elems];
     unsigned int* h_out_gpu = new unsigned int[num_elems];
 
+    for (int j = 0; j < num_elems; j++)
+    {
+        h_in[j] = (num_elems - 1) - j;
+        h_in_rand[j] = rand() % num_elems;
+    }
+
     unsigned int* d_in;
     unsigned int* d_out;
     cudaMalloc(&d_in, sizeof(unsigned int) * num_elems);
     cudaMalloc(&d_out, sizeof(unsigned int) * num_elems);
-    cudaMemcpy(d_in, h_in, sizeof(unsigned int) * num_elems, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_in, h_in_rand, sizeof(unsigned int) * num_elems, cudaMemcpyHostToDevice);
     start = std::clock();
     radix_sort(d_out, d_in, num_elems);
     double gpu_duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
