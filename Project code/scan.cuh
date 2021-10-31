aecbcde5170721f489d17fcd34d3fcb488e0ef7f
Marc André Raymond Raffy <kpf905@alumni.ku.dk>
2021-10-31 19:03:39 +0100

    gh

--- Project code/scan.cuh
+++ Project code/scan.cuh
@@ -57,6 +57,7 @@ __global__ void prescan(unsigned int *g_odata, unsigned int *g_idata, int n) {
 } 
 
 void prefixsumScan(unsigned int *d_out, unsigned int *d_in, int length) {
+    std::cout << "i am a dumbass" << " ";
 	const int blocks = length / ELEMENTS_PER_BLOCK;
     prescan<<<blocks, THREADS_PER_BLOCK>>>(d_out, d_in, ELEMENTS_PER_BLOCK);
 }
