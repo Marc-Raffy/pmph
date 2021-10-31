0710123699eae55e1828afc51c82af3c67cc369f
Marc André Raymond Raffy <kpf905@alumni.ku.dk>
2021-10-31 21:24:16 +0100

    wc

--- Project code/scan.cuh
+++ Project code/scan.cuh
@@ -65,7 +65,9 @@ __global__ void prescan(unsigned int *g_odata, unsigned int *g_idata, int n)
     __syncthreads(); 
     g_odata[ai] = temp[ai + bankOffsetA];
     g_odata[bi] = temp[bi + bankOffsetB];
-    
+    #if __CUDA_ARCH__ >= 200
+    printf("3");
+    #endif
     
 } 
 
