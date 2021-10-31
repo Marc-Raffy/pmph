15ec7a6f13158bed0897f2b4d2d3678413a72076
Marc André Raymond Raffy <kpf905@alumni.ku.dk>
2021-10-31 19:37:41 +0100

    fh

--- Project code/scan.cuh
+++ Project code/scan.cuh
@@ -59,5 +59,5 @@ void prefixsumScan(unsigned int *d_out, unsigned int *d_in, int length) {
     std::cout << "prefix sum scan was called";
     unsigned int shared_mem = BLOCK_SIZE + (BLOCK_SIZE >> LOG_NUM_BANKS);
 	const int blocks = length / ELEMENTS_PER_BLOCK;
-    prescan<<<blocks, THREADS_PER_BLOCK, sizeof(float) * shared_mem>>>();
+    prescan<<<blocks, 128, sizeof(unsigned int) * shared_mem>>>();
 }
